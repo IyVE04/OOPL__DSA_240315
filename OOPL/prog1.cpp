dca042a65f141e500ca613b6f80db7f33655fcf8
IyVE04 <iycaaus763@gmail.com>
2025-12-02 18:09:17 +0530

    upd 4

--- OOPL/prog1.cpp
+++ OOPL/prog1.cpp
@@ -1,48 +1,37 @@
 #include <iostream>
 #include <string>
 
-// 1. Class Definition: Dog
-// This class is a simple blueprint for a Dog object.
+using namespace std;
+
 class Dog {
 private:
-    // Simple private data members
-    std::string name;
+    string name;
     int age;
 
 public:
-    // Simple Constructor to initialize the object
-    Dog(const std::string& n, int a) {
+    Dog(const string& n, int a) {
         name = n;
         age = a;
-        std::cout << "Dog object created: " << name << ", Age: " << age << std::endl;
+        cout << "Dog object created: " << name << ", Age: " << age << endl;
     }
 
-    // Method: The object's behavior
     void makeSound() const {
-        std::cout << name << " says: Woof! Woof!" << std::endl;
+        cout << name << " says: Woof! Woof!" << endl;
     }
 
-    // A simple accessor (getter) method
-    std::string getName() const {
+    string getName() const {
         return name;
     }
 };
 
-// Main function to demonstrate creating and using a simple object
 int main() {
-    // 2. Object Creation
-    // Create an object named 'myDog' from the Dog class blueprint.
     Dog myDog("Sparky", 3);
 
-    std::cout << "\n--- Using the Dog Object ---" << std::endl;
+    cout << "\n--- Using the Dog Object ---" << endl;
 
-    // Call the makeSound method on the 'myDog' object
     myDog.makeSound();
+    cout << "My dog's name is " << myDog.getName() << "." << endl;
 
-    // Call the getName method
-    std::cout << "My dog's name is " << myDog.getName() << "." << std::endl;
-
-    // Create another dog object
     Dog neighborhoodDog("Max", 5);
     neighborhoodDog.makeSound();
 
